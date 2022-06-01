28e11674005d5e26f1bfe882b9ab71972b71d1d9
Viktoriia <krjuchenkova@gmail.com>
2022-06-01 13:09:51 +0300

    implement Stack class with methods push, pop, getLength, isFull, isEmpty, peek

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// queue - first in first out
+// Queue - FIFO: first in first out
 
 class Queue {
     constructor(collection) {
@@ -25,4 +25,50 @@ class Queue {
 }
 
 const item = new Queue([1, 2, 3, 4]);
+
+// - Stack - LIFO: last in first out
+
+class Stack {
+    constructor(collection) {
+        this.collection = collection
+    }
+
+//push - insert an element on the top of the stack
+
+    push(el) {
+        this.collection.unshift(el);
+        return this.collection;
+    }
+
+//pop - delete element from the top of the stack and return it
+
+    pop() {
+        return this.collection.pop()
+    }
+//length - get the size of the stack
+
+    getLength() {
+        return this.collection.length;
+    }
+
+// peek - return the top element of the stack without deleting it
+
+    peek() {
+        return this.collection.length ? this.collection[0] : null;
+    }
+
+// isEmpty - check if the stack is empty
+
+    isEmpty() {
+        return this.collection.length === 0;
+    }
+
+// isFull - check if the stack is full
+
+    isFull() {
+        return this.collection.length !== 0;
+    }
+}
+
+const stack = new Stack([1, 2, 3, 4, 5, 6, 7]);
 
