b732eace49d75b76c9822770b23cad4b85e6a1bd
Viktoriia <krjuchenkova@gmail.com>
2022-06-01 12:36:26 +0300

    fix dequeue method, add getSize method

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+// queue - first in first out
+
 class Queue {
     constructor(collection) {
         this.collection = collection
     }
+
+    // - enqueue method insert an element to the end of the queue
     enqueue(el) {
-        return this.collection.push(el)
+        this.collection.push(el);
+        return this.collection
     }
-    dequeue(el) {
-        return this.collection.shift(el)
+
+    // - dequeue method delete element from the beginning of the queue
+    dequeue() {
+        this.collection.shift();
+        return this.collection
+    }
+
+    // length of collection 
+
+    getSize() {
+        return this.collection.length;
     }
 }
+
+const item = new Queue([1, 2, 3, 4]);
+
