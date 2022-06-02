4cf1c5cef5341478bd09280786e41ed982ccda5c
Viktoriia <krjuchenkova@gmail.com>
2022-06-03 00:13:34 +0300

    add weakmap with methods

--- set.js
+++ set.js
@@ -29,8 +29,26 @@ class Set {
 
 const list = new Set();
 
+
 class WeakSet {
     collection = [];
 
-
+    isObj(obj) {
+        return obj !== null && obj.constructor.name === "Object";
+    }
+    add(item) { 
+        if (!this.isObj(item) || this.has(item)) {
+            return null;
+        }
+        this.collection.push(item)
+    }
+    has() { 
+        return this.collection.indexOf !== -1;
+    }
+    delete(item) {
+        this.collection = this.collection.filter(el => el !== item)
+    }
+    clear() {
+        this.collection = [];
+    }
 }
