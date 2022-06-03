d1c0d3609ce8cf35a3097b807e71f781008e2c31
Viktoriia <krjuchenkova@gmail.com>
2022-06-03 22:12:18 +0300

    correct coding styles

--- set.js
+++ set.js
@@ -1,6 +1,5 @@
 class Set {
     collection = [];
-
     add(el) { 
         if (this.collection.length) {
           return this.has(el) ? null : this.collection.push(el);
@@ -19,31 +18,29 @@ class Set {
     has(el) { 
         return this.collection.indexOf(el) !== -1;
     }
+
     size() {
         return this.collection.length;
     }
-    keys() {
+
+    values() {
         return { ...this.collection };
     }
+
     union(set) {
         let result = [];
         let arr = Array.from(set.collection);
-        result = this.collection
-            .filter(el => arr
-                .indexOf(el) !== -1);
+        result = this.collection.filter(el => arr.indexOf(el) !== -1);
         return result;
-     }
+    }
+
     difference(set) {
         let result = [];
         let arr = Array.from(set.collection);
-        result = this.collection
-            .filter(el => arr
-                .indexOf(el) === -1)
-            .concat(arr
-                .filter(el => this.collection
-                    .indexOf(el) === -1));
+        result = this.collection.filter(el => arr.indexOf(el) === -1).concat(arr.filter(el => this.collection.indexOf(el) === -1));
         return result;
-     }
+    }
+
     subset(set) {
         let arr = Array.from(set.collection);
         return arr.every(el => this.collection.includes(el));
@@ -52,30 +49,31 @@ class Set {
 
 class WeakSet {
     collection = [];
-
-    add(item) { 
+    add(item) {
         if (this.isObj(item)) {
             if (this.collection.length) {
-              this.has(item) ? null : this.collection.push(item);
+                this.has(item) ? null : this.collection.push(item);
             }
             this.collection.push(item);
         } else {
             return null;
         }
     }
+    
     isObj(obj) {
         return obj !== null && obj.constructor.name === "Object";
     }
-    has() { 
+
+    has() {
         return this.collection.indexOf !== -1;
     }
+
     delete(idx) {
         if (idx < 0 || idx > this.collection.length) {
             return null;
         }
         this.collection = this.collection.filter((el, i) => i !== idx);
     }
-    
 }
 
 
